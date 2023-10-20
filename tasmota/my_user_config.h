7f2cb71946211b66174df3b1005204159a660a37
ma_jk <marco_jk@hotmail.com>
2023-10-20 16:18:05 +0200

    invia email a indirizzo cablato su allarme

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -508,6 +508,7 @@
 
 // -- Optional modules ----------------------------
 #define USE_EBCMINICLIMA
+#define USE_SENDMAIL
 //#define ROTARY_V1                                // Add support for Rotary Encoder as used in MI Desk Lamp (+0k8 code)
   #define ROTARY_MAX_STEPS     10                // Rotary step boundary
 //#define USE_SONOFF_RF                            // Add support for Sonoff Rf Bridge (+3k2 code)
